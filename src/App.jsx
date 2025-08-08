aabcbb8418051f75bb5dceeab3649fa4b4ea9d35
suman shradha <shradhasuman852216@gmail.com>
2025-08-08 16:08:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='flex justify-center items-center'>
+      <h2>NetFlix GPT</h2>
+    </div>
+  )
+}
+
+export default App
